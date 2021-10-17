d43e1d8ca11e0e91131320f6880f8a8f651b9e00
RolandoPranataAR <rolandopranata@gmail.com>
2021-10-17 23:33:19 +0700

    Add crud for table category

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+// import mongoose
+const mongoose = require('mongoose');
+const methodOvveride = require('method-override');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// Mongoose connection
+mongoose.connect('mongodb://localhost:27017/db_mern20', {
+    useNewUrlParser: true,
+});
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 // admin router 
 const adminRouter = require('./routes/admin');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(methodOvveride('_method'));
 
 app.use(logger('dev'));
 app.use(express.json());
